5253ccf61345094a7cb38223a24e805ad70679c9
강혁준 <domohit5124@naver.com>
2024-05-16 09:56:01 +0900

    kakaomap 크기 수정

--- trip-vue/src/components/search/KakaoMapSearch.vue
+++ trip-vue/src/components/search/KakaoMapSearch.vue
@@ -1,46 +1,44 @@
 <script setup>
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
 // import { getSido } from "@/api/search";
 
-// import { KakaoMap, KakaoMapMarker } from "vue3-kakao-maps";
-// const coordinate = {
-//   lat: 37.566826,
-//   lng: 126.9786567,
-// };
+import { KakaoMap, KakaoMapMarker } from "vue3-kakao-maps";
+const coordinate = {
+  lat: 37.566826,
+  lng: 126.9786567,
+};
 
-// console.log(
-//   getSido(
-//     async (response) => {
-//       await console.log(response);
-//     },
-//     async (error) => {
-//       await console.log(error);
-//     }
-//   )
-// );
+const mapWidth = ref(0);
+const mapHeight = ref(0);
+const isLoaded = ref(false);
 
 onMounted(() => {
   const container = document.querySelector("#container");
-  console.log(container);
-  console.log(container.offsetWidth);
-  console.log(container.offsetHeight);
+  // console.log(container);
+  // console.log(container.offsetWidth);
+  // console.log(container.offsetHeight);
+  mapWidth.value = container.offsetWidth * 0.95;
+  mapHeight.value = container.offsetHeight * 0.95;
+  isLoaded.value = true;
 });
 </script>
 
 <template>
   <div class="flex flex-row border-none w-full h-full">
-    <div id="container" class="w-full h-full">
-      <!-- <KakaoMap
-        width="70rem"
-        height="45.5rem"
-        :lat="coordinate.lat"
-        :lng="coordinate.lng"
-        :draggable="true"
-      >
-        <KakaoMapMarker :lat="coordinate.lat" :lng="coordinate.lng"></KakaoMapMarker>
-      </KakaoMap> -->
+    <div id="container" class="w-full h-full flex justify-center items-center">
+      <template v-if="isLoaded">
+        <KakaoMap
+          :width="mapWidth + 'px'"
+          :height="mapHeight + 'px'"
+          :lat="coordinate.lat"
+          :lng="coordinate.lng"
+          :draggable="true"
+        >
+          <KakaoMapMarker :lat="coordinate.lat" :lng="coordinate.lng"></KakaoMapMarker>
+        </KakaoMap>
+      </template>
     </div>
-    <div class="w-52 bg-zinc-600">side</div>
+    <div class="w-[18rem] bg-zinc-600">side</div>
   </div>
 </template>
 
