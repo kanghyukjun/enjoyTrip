0c634e42a52c42b49a38aa51a76e7c020d98208c
hyukjun kang <domohit5124@gmail.com>
2024-05-20 22:04:45 +0900

    [design] 유저가 저장한 여행 목록 디자인 추가

--- trip-vue/src/components/userpage/UserSavedTripList.vue
+++ trip-vue/src/components/userpage/UserSavedTripList.vue
@@ -3,17 +3,22 @@ import UserSavedTripListItem from "@/components/userpage/UserSavedTripListItem.v
 </script>
 
 <template>
-  <div class="w-full h-full flex flex-col items-center justify-center bg-pink-300">
-    <div class="w-5/6 h-[3rem] flex flex-row items-center justify-start bg-blue-400 font-kor">
-      <p class="ml-3 text-gray-700 font-bold text-2xl">내 여행 코스</p>
+  <div class="w-full h-full flex flex-col items-center justify-center">
+    <div class="w-5/6 h-[3rem] flex flex-row items-center justify-start font-kor">
+      <p class="ml-3 text-gray-700 font-bold text-2xl mt-10">내 여행 코스</p>
     </div>
-    <div
-      class="w-5/6 h-full flex flex-col items-center justify-center bg-green-300 font-kor transition-all"
-    >
+    <div class="w-5/6 h-full flex flex-col items-center justify-center font-kor transition-all">
       <div
-        class="w-11/12 h-5/6 flex flex-col items-center justify-start bg-pink-400 rounded-md overflow-y-auto"
+        class="w-11/12 h-5/6 flex flex-col items-center justify-start rounded-md overflow-y-auto"
       >
-        <UserSavedTripListItem />
+        <UserSavedTripListItem class="mt-3" />
+        <UserSavedTripListItem class="mt-3" />
+        <UserSavedTripListItem class="mt-3" />
+        <UserSavedTripListItem class="mt-3" />
+        <UserSavedTripListItem class="mt-3" />
+        <UserSavedTripListItem class="mt-3" />
+        <UserSavedTripListItem class="mt-3" />
+        <UserSavedTripListItem class="mt-3" />
       </div>
     </div>
   </div>
--- trip-vue/src/components/userpage/UserSavedTripListItem.vue
+++ trip-vue/src/components/userpage/UserSavedTripListItem.vue
@@ -1,7 +1,22 @@
 <script setup></script>
 
 <template>
-  <div class="w-full h-[7rem] bg-gray-300 rounded-md"></div>
+  <div
+    class="w-full h-[7rem] flex flex-col items-center justify-center rounded-md border-2 border-gray-300 hover:border-4 transition-all"
+  >
+    <div class="w-full h-5/6 flex flex-row items-start justify-center font-kor text-gray-700">
+      <div class="w-8/12 h-full flex flex-col items-start justify-center">
+        <p class="font-bold text-xl ml-3">제목</p>
+        <p class="text-md ml-3">유저가 지정한 제목</p>
+        <p class="mt-2 ml-3 text-sm">2024-05-20 ~ 2024-05-26</p>
+      </div>
+      <div class="w-4/12 h-full flex flex-col items-end justify-center">
+        <div class="w-[5.5rem] h-[5rem] rounded-md bg-pink-400 mr-3">
+          div 대신 이미지를 (만약 넣는다면)
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
