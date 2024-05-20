9818e7c9f1eba1f121a9a3f30c29eea456d3e66f
강혁준 <domohit5124@naver.com>
2024-05-20 17:47:22 +0900

    [design] 유저 페이지 여행 계획 디자인 수정

--- trip-vue/src/components/userpage/UserSavedTripList.vue
+++ trip-vue/src/components/userpage/UserSavedTripList.vue
@@ -1,7 +1,22 @@
-<script setup></script>
+<script setup>
+import UserSavedTripListItem from "@/components/userpage/UserSavedTripListItem.vue";
+</script>
 
 <template>
-  <div>user가 저장한 여행지 리스트</div>
+  <div class="w-full h-full flex flex-col items-center justify-center bg-pink-300">
+    <div class="w-5/6 h-[3rem] flex flex-row items-center justify-start bg-blue-400 font-kor">
+      <p class="ml-3 text-gray-700 font-bold text-2xl">내 여행 코스</p>
+    </div>
+    <div
+      class="w-5/6 h-full flex flex-col items-center justify-center bg-green-300 font-kor transition-all"
+    >
+      <div
+        class="w-11/12 h-5/6 flex flex-col items-center justify-start bg-pink-400 rounded-md overflow-y-auto"
+      >
+        <UserSavedTripListItem />
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
---
+++ trip-vue/src/components/userpage/UserSavedTripListItem.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div class="w-full h-[7rem] bg-gray-300 rounded-md"></div>
+</template>
+
+<style scoped></style>
