a533e6c61e774d40df5a8cbd3dd1e01260fe90a2
kangtaekyung <rkdxorud3939@naver.com>
2024-05-23 09:17:52 +0900

    [refactor] 유저가 작성한 글 목록 조회 시 page, word 파라미터 제거

--- trip-back/src/main/java/com/ssafy/board/service/BoardServiceImpl.java
+++ trip-back/src/main/java/com/ssafy/board/service/BoardServiceImpl.java
@@ -57,6 +57,11 @@ public class BoardServiceImpl implements BoardService{
     }
 
     @Override
+    public ResponseEntity<Map<String, Object>> getHitBoard() {
+        return null;
+    }
+
+    @Override
     public ResponseEntity<Map<String, Object>> get(int pgno, String word) {
         Map<String, Object> resultMap = new HashMap<>();
         log.debug("[BoardService] 게시글 목록 조회");
@@ -68,13 +73,11 @@ public class BoardServiceImpl implements BoardService{
     }
 
     @Override
-    public ResponseEntity<Map<String, Object>> getUserBoard(String loginId, int pgno, String word, HttpServletRequest request) {
+    public ResponseEntity<Map<String, Object>> getUserBoard(String loginId, HttpServletRequest request) {
         Map<String, Object> resultMap = new HashMap<>();
         log.debug("[BoardService] 유저의 게시글 목록 조회 - {}", loginId);
-        List<BoardSimpleResponseDto> articles = boardMapper.getUserBoard(userMapper.getByLoginId(loginId).getId(), (pgno-1)*8, 8, word);
-        int count = boardMapper.count(word);
+        List<BoardSimpleResponseDto> articles = boardMapper.getUserBoard(userMapper.getByLoginId(loginId).getId());
         resultMap.put("articles", articles);
-        resultMap.put("count", count);
         return ResponseEntity.ok().body(resultMap);
     }
 
