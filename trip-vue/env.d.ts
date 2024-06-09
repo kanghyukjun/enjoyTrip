9e89699da200c2a5a2beb0a41dfff7425a7d5db2
hyukjun kang <domohit5124@gmail.com>
2024-06-09 17:06:19 +0900

    [build] typescript 및 vue 관련 typescript 라이브러리 추가

---
+++ trip-vue/env.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="vite/client" />
+interface ImportMeta {
+  env: {
+    VITE_KAKAO_OPEN_API_KEY?: string;
+    VITE_PJT_API_URL?: string;
+    VITE_BOARD_MAX_PER_PAGE?: string;
+    VITE_BOARD_MAX_CONTENT_SIZE?: string;
+    VITE_BOARD_MAX_TITLE_SIZE?: string;
+    VITE_MAX_FILE_SIZE?: string;
+  };
+}
--- trip-vue/src/components/board/BoardDetail.vue
+++ trip-vue/src/components/board/BoardDetail.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 import VButton from "@/components/common/item/VButton.vue";
 import VSpotListItem from "@/components/common/item/VSpotListItem.vue";
 
